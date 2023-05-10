05c6591126ed82d56270e111c07b36be91dfb8e9
hvtien3 <hvantiendh@gmail.com>
2023-05-10 07:48:59 +0000

    Code event hub

---
+++ EventHubQuickStart/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Create a blob container client that the processor will use
+BlobContainerClient storageClient = new BlobContainerClient(
+    new Uri("https://sahvtien3lab.blob.core.windows.net/democontainer1"),
+    new DefaultAzureCredential()
+);
+
+var processor = new EventProcessorClient(
+    storageClient,
+    EventHubConsumerClient.DefaultConsumerGroupName,
+    "hvtien3eventhubns.servicebus.windows.net",
+    "hvtien3eventhub",
+    new DefaultAzureCredential()
+);
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+// Start the processing
+await processor.StartProcessingAsync();
+
+// Wait for 30 seconds for the events to be processed
+await Task.Delay(TimeSpan.FromSeconds(30));
+
+// Stop the processing
+await processor.StopProcessingAsync();
+
+Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    // Write the body of the event to the console window
+    Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+    return Task.CompletedTask;
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    return Task.CompletedTask;
+}
---
+++ EventHubsSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// number of events to be sent to the event hub
+int numOfEvents = 3;
+
+// The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when events are being published or read regularly
+EventHubProducerClient producerClient = new EventHubProducerClient(
+    "hvtien3eventhubns.servicebus.windows.net",
+    "myeventhubtestcapture",
+    new DefaultAzureCredential());
+
+// Create batch of events
+using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+for (int i = 1; i <= numOfEvents; i++){
+    if(!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}")))){
+        throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+    }
+}
+
+try{
+    // Use the producer client to send the batch of events to the event hub
+    await producerClient.SendAsync(eventBatch);
+     Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+}
+finally
+{
+    await producerClient.DisposeAsync();
+}
